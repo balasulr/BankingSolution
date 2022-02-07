fb253e6a32cce3e6e06b3c213c9000e7c8d851ab
Lakshmi Balasubramaniam <lrb697@gmail.com>
2022-02-07 16:22:14 -0500

    Adding Inheritance and Savings Account

--- BankingProject/Program.cs
+++ BankingProject/Program.cs
@@ -5,23 +5,31 @@ using System;
 namespace BankingProject {
     class Program {
         static void Main(string[] args) {
-            // New instance of a acocunt
-            Account acct1 = new Account(); // Here, had an error since the namespaces were different
-            acct1.Deposit(500);
-            acct1.Withdraw(200);
-            acct1.Debug();
-            //acct1.Withdraw(600);
-            //acct1.Debug();
+            // Adding a Savings Account
+            Savings sav1 = new Savings();
+            sav1.Deposit(100);
+            sav1.CalculateAndPayInterest(1); // Paying 1% per month
+            sav1.CalculateAndPayInterest(1);
+            sav1.Debug();
 
-            Account acct2 = new Account();
-            acct2.Description = "My Second Account";
-            acct2.Deposit(1000);
-            acct2.Withdraw(350);
-            acct2.Debug();
-            // Transfer $100 from Account 2 to Account 1
-            acct2.Transfer(100, acct1);
-            acct1.Debug();
-            acct2.Debug();
+
+            //// New instance of a acocunt
+            //Account acct1 = new Account(); // Here, had an error since the namespaces were different
+            //acct1.Deposit(500);
+            //acct1.Withdraw(200);
+            //acct1.Debug();
+            ////acct1.Withdraw(600);
+            ////acct1.Debug();
+
+            //Account acct2 = new Account();
+            //acct2.Description = "My Second Account";
+            //acct2.Deposit(1000);
+            //acct2.Withdraw(350);
+            //acct2.Debug();
+            //// Transfer $100 from Account 2 to Account 1
+            //acct2.Transfer(100, acct1);
+            //acct1.Debug();
+            //acct2.Debug();
 
         }
     }
---
+++ BankingProject/Savings.cs
@@ -0,0 +1,24 @@
+﻿using Banking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingProject {
+
+    public class Savings : Account { // Savings is inheriting Account
+        // What makes Saving account different from Account is paying Interest
+
+        // Properties
+        public decimal InterestRate { get; set; } = 0.12m;
+
+        // Method to calculate how much Interest & put it into Account
+        public void CalculateAndPayInterest(int months) {
+            // Calculating how much interest to be paid
+            decimal interest = Balance * (InterestRate / 12) * months;
+            
+            Deposit(interest);
+        }
+    }
+}
